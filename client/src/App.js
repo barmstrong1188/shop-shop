f9a421cfb3cbbb016592607be646533a304c2d95
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-10-25 06:36:21 -0400

    deploying

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
