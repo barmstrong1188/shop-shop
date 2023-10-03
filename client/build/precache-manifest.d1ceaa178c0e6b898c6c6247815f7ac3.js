08e42d262c21f326dc9476475b589e9c7b6ab0b5
Brandon Armstrong <barmstrong1188@gmail.com>
2023-10-03 17:54:52 -0400

    added dyno and updated to heroku 22

---
+++ client/build/precache-manifest.d1ceaa178c0e6b898c6c6247815f7ac3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21379901143bb262a5c3b940d64e5301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3444af3f9649bc6db264",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "d18cca4aeb67d37703e4",
+    "url": "/static/js/2.d99691ed.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.d99691ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3444af3f9649bc6db264",
+    "url": "/static/js/main.9cdf427e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9cdf427e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
